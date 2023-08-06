a7cf299f3f9adb00654876765dc5a90f4332e65e
CHINMAY PAREEK <132579337+aamadmi3439@users.noreply.github.com>
2023-08-06 18:42:29 +0530

    Create bubblesort.py

---
+++ bubblesort.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 11 08:33:50 2023
+
+@author: chinmay pareek
+"""
+
+import time
+
+def bubble_sort(data, drawrectangle, delay):
+    for i in range(len(data)-1):
+        for j in range(len(data)-1):
+            if data[j] > data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j]
+                drawrectangle(data, ['blue' if x == j or x == j+1 else 'red' for x in range(len(data))] )
+                time.sleep(delay)
+    drawrectangle(data, ['blue' for x in range(len(data))])
